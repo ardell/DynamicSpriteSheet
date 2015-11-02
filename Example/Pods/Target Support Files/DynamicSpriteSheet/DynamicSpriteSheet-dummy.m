42a6f52b3278a057e7270c5fdbcbeab4e7236a64
Jason Ardell <ardell@gmail.com>
2015-11-02 11:43:48 -0700

    Add generated files.

---
+++ Example/Pods/Target Support Files/DynamicSpriteSheet/DynamicSpriteSheet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DynamicSpriteSheet : NSObject
+@end
+@implementation PodsDummy_DynamicSpriteSheet
+@end
---
+++ Example/Pods/Target Support Files/DynamicSpriteSheet/DynamicSpriteSheet-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double DynamicSpriteSheetVersionNumber;
+FOUNDATION_EXPORT const unsigned char DynamicSpriteSheetVersionString[];
+
